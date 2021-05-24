b4164e3b7d884a85cd10b806f2a4163d4fe4fc52
Guiaochino <chinotiamzon@gmail.com>
2021-05-24 14:54:05 +0800

    Image Logo added and java Configured Incomplete

--- app/src/main/java/com/example/filipino_slang/MainActivity.java
+++ app/src/main/java/com/example/filipino_slang/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.filipino_slang;
 
+import android.view.View;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Toggle_Click(View view) {
+    }
+
+    public void onDefaultToggleClick(View view) {
+        Toast.makeText( context: this, text: "DefaultToggle",Toast.LENGTH_SHORT).show();
     }
 }
