4e5c1e99c6247465aa113ce6744506bc6469be09
Clio Andrei J. Gomez <clioandreigomez1116000@gmail.com>
2021-06-01 10:54:05 +0800

    MenuScreen to GameScreen

--- app/src/main/java/com/example/filipino_slang/MainActivity.java
+++ app/src/main/java/com/example/filipino_slang/MainActivity.java
@@ -1,21 +1,33 @@
 package com.example.filipino_slang;
 
+import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button playGameButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    /*public void onDefaultToggleClick(View view) {
-        Toast.makeText( this, "DefaultToggle", Toast.LENGTH_SHORT).show();
-    }*/
+        playGameButton = (Button) findViewById(R.id.btnPlayGame);
+        playGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGameScreen();
+            }
+        });
+    }
+    public void openGameScreen(){
+        Intent playGame = new Intent(this, GameScreen.class);
+        startActivity(playGame);
+    }
 
     public void onCustomToggleClick(View view) {
         Toast.makeText(this, "CustomToggle", Toast.LENGTH_SHORT).show();
